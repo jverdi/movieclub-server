969b165b270b81096d76e8ee4ad0ad9adfbfde1f
Jared Verdi <jared@jaredverdi.com>
2016-04-16 18:09:57 -0400

    Minor updates

--- Package.swift
+++ Package.swift
@@ -2,13 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "movieclub-server",
-    targets: [
-        Target(
-            name: "movieclub-server",
-            dependencies: []
-        )
-    ],
     dependencies: [
         .Package(url: "https://github.com/nestproject/Frank.git", majorVersion: 0, minor: 3),
-    ],
+    ]
 )

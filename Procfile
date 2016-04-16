2d87757f1d992e423c0615032497d6756ac4535d
Jared Verdi <jared@jaredverdi.com>
2016-04-16 17:51:41 -0400

    Movie Club Server initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: Hello --workers 1 --bind 0.0.0.0:$PORT
---
+++ Sources/movieclub-server/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  movieclub-server
+//
+//  Created by Jared Verdi on 4/16/16.
+//  Copyright © 2016 Jared Verdi. All rights reserved.
+//
+
+import Frank
+
+get { _ in
+  return "Hello World"
+}
